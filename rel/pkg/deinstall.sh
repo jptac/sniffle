49663364953a4eaa6f05f817d47a0dcd26efa3e3
Heinz N. Gies <heinz@licenser.net>
2012-11-29 05:54:33 +0100

    Experimenting with hot updateing packages.

--- rel/pkg/deinstall.sh
+++ rel/pkg/deinstall.sh
@@ -2,12 +2,8 @@
 
 case $2 in
     DEINSTALL)
-	echo "Stopping Sniffle service."
-	svcadm disable network/sniffle
 	;;
     POST-DEINSTALL)
-	echo "Removing Sniffle service."
-	svccfg delete network/sniffle
 	echo "Please beware that database and logfiles have not been"
 	echo "deleted! Neither have the sniffle user or gorup."
 	echo "If you don't need them any more remove the directories:"
--- rel/pkg/install.sh
+++ rel/pkg/install.sh
@@ -5,35 +5,7 @@ GROUP=$USER
 
 case $2 in
     PRE-INSTALL)
-	if grep "^$GROUP:" /etc/group > /dev/null 2>&1
-	then
-	    echo "Group already exists, skipping creation."
-	else
-	    echo Creating snarl group ...
-	    groupadd $GROUP
-	fi
-	if id $USER > /dev/null 2>&1
-	then
-	    echo "User already exists, skipping creation."
-	else
-	    echo Creating snarl user ...
-	    useradd -g $GROUP -d /var/db/snarl -s /bin/false $USER
-	fi
-	echo Creating directories ...
-	mkdir -p /var/db/sniffle/ring
-	mkdir -p /var/db/sniffle/ipranges
-	mkdir -p /var/db/sniffle/packages
-	mkdir -p /var/db/sniffle/datasets
-	chown -R sniffle:sniffle /var/db/sniffle
-	mkdir -p /var/log/sniffle/sasl
-	chown -R sniffle:sniffle /var/log/sniffle
 	;;
     POST-INSTALL)
-	echo Importing service ...
-	svccfg import /opt/local/sniffle/etc/sniffle.xml
-	echo Trying to guess configuration ...
-	IP=`ifconfig net0 | grep inet | awk -e '{print $2}'`
-	sed --in-place=.bak -e "s/127.0.0.1/${IP}/g" /opt/local/sniffle/etc/vm.args
-	sed --in-place=.bak -e "s/127.0.0.1/${IP}/g" /opt/local/sniffle/etc/app.config
 	;;
 esac
