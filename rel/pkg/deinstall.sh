d922fb376d2f718031181d9db97776378a80cc2d
Heinz N. Gies <heinz@licenser.net>
2012-12-12 17:39:09 +0100

    Fixed uninstall script.

--- rel/pkg/deinstall.sh
+++ rel/pkg/deinstall.sh
@@ -3,11 +3,9 @@
 case $2 in
     DEINSTALL)
 	echo "Stopping Sniffle service."
-	svcadm disable network/sniffle
 	;;
     POST-DEINSTALL)
 	echo "Removing Sniffle service."
-	svccfg delete network/sniffle
 	echo "Please beware that database and logfiles have not been"
 	echo "deleted! Neither have the sniffle user or gorup."
 	echo "If you don't need them any more remove the directories:"
