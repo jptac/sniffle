0a26dff1cc6c4d80cd5533c96f2b10f59fc4aa7d
Heinz N. Gies <heinz@licenser.net>
2013-01-07 20:35:15 +0100

    Fixed logging.

--- apps/sniffle/src/sniffle_vm_state.erl
+++ apps/sniffle/src/sniffle_vm_state.erl
@@ -31,6 +31,8 @@ uuid(UUID, Vm) ->
 %%    Vm#vm{alias = Alias}.
 
 log(Time, Log, Vm) ->
+    LogEntry = [{<<"date">>, Time},
+                {<<"log">>, Log}],
     jsxd:update(<<"log">>,
                 fun(Log0) ->
                         Log1 = case length(Log0) of
@@ -42,7 +44,7 @@ log(Time, Log, Vm) ->
                                end,
                         ordsets:add_element([{<<"date">>, Time},
                                              {<<"log">>, Log}], Log1)
-                end, Vm).
+                end, [LogEntry], Vm).
 
 hypervisor(Hypervisor, Vm) ->
     jsxd:set(<<"hypervisor">>, Hypervisor, Vm).
