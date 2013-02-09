3bef6af176de9e7c565e5ae6a15a134f9611460e
Heinz N. Gies <heinz@licenser.net>
2013-02-09 06:15:59 +0100

    Added more code.

--- apps/sniffle/src/sniffle_dtrace.erl
+++ apps/sniffle/src/sniffle_dtrace.erl
@@ -4,7 +4,17 @@
 
 -export(
    [
-    add/2
+    get/1,
+    add/2,
+    delete/1
+   ]
+  ).
+
+-ignore_xref(
+   [
+    get/1,
+    add/2,
+    delete/1
    ]
   ).
 
@@ -13,20 +23,28 @@ add(Name, Script) ->
     do_write(UUID, create, [Name, Script]),
     {ok, UUID}.
 
+get(UUID) ->
+    sniffle_entity_read_fsm:start(
+      {sniffle_dtrace_vnode, sniffle_dtrace},
+      get, UUID
+     ).
+
+delete(UUID) ->
+    do_write(UUID, delete).
 
 %%%===================================================================
 %%% Internal Functions
 %%%===================================================================
 
-%% -spec do_write(VM::fifo:uuid(), Op::atom()) -> not_found | ok.
+-spec do_write(VM::fifo:uuid(), Op::atom()) -> not_found | ok.
 
-%% do_write(VM, Op) ->
-%%     case sniffle_entity_write_fsm:write({sniffle_vm_vnode, sniffle_vm}, VM, Op) of
-%%         {ok, not_found} ->
-%%             not_found;
-%%         R ->
-%%             R
-%%     end.
+do_write(VM, Op) ->
+    case sniffle_entity_write_fsm:write({sniffle_vm_vnode, sniffle_vm}, VM, Op) of
+        {ok, not_found} ->
+            not_found;
+        R ->
+            R
+    end.
 
 -spec do_write(VM::fifo:uuid(), Op::atom(), Val::term()) -> not_found | ok.
 
