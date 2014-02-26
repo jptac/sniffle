628b7d96973d041cb69fcc03a742bdf86c1888b4
Heinz N. Gies <heinz@licenser.net>
2014-02-25 23:37:55 -0500

    Added locking to vm creation.

--- apps/sniffle/src/sniffle_util.erl
+++ apps/sniffle/src/sniffle_util.erl
@@ -1,5 +1,7 @@
 -module(sniffle_util).
--export([ensure_str/1]).
+-export([ensure_str/1, node_for/1]).
+
+-ignore_xref([node_for/1]).
 
 ensure_str(V) when is_atom(V) ->
     atom_to_list(V);
@@ -9,3 +11,9 @@ ensure_str(V) when is_integer(V) ->
     integer_to_list(V);
 ensure_str(V) when is_list(V) ->
     V.
+
+node_for(Key) ->
+    {ok, Ring} = riak_core_ring_manager:get_my_ring(),
+    CHash = riak_core_ring:chash(Ring),
+    [{_, Node}] = chash:successors(chash:key_of(Key), CHash, 1),
+    Node.
